5b5a9e95a13ce4b92973670e076dbfb4a378fb83
Liam Spatola <liam.spatola@gmail.com>
2024-11-17 12:50:36 +1100

    Created app.py and built a base flask app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_socketio import SocketIO
+
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+
+@app.route("/")
+def index():
+    return "Page under construction"
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
