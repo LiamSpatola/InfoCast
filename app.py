5c3390a7328c424d80f738a6526166a459934f3a
Liam Spatola <liam.spatola@gmail.com>
2024-11-17 12:59:34 +1100

    Updated app.py to add basic socket event handlers

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from os import environ
 
 from dotenv import load_dotenv
 from flask import Flask
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send
 
 
 # Configuring the app and websockets
@@ -20,6 +20,22 @@ app.config["SECRET_KEY"] = environ.get("SECRET_KEY", default="super-secret-key")
 @app.route("/")
 def index():
     return "Page under construction"
+
+
+@socketio.on("broadcast")
+def handle_broadcast(msg):
+    # Handling a broadcast to the display
+    send(msg, broadcast=True)
+
+
+@socketio.on("connect")
+def handle_connect():
+    print("Client connected")
+
+
+@socketio.on("disconnect")
+def handle_disconnect():
+    print("Client disconnected")
 
 
 if __name__ == "__main__":
